6577cc4a3948d2b59f637d8fa7718c93c5353be6
Edzass <93673275+Edzass@users.noreply.github.com>
2026-04-28 18:16:07 +0300

    sagatave

---
+++ 1_firstweekday copy.py
@@ -0,0 +1,14 @@
+import calendar
+
+
+print("Kāda ir pirmā nedēļas diena?")
+print("Pirmdiena(1) ; Otrdiena(2) ; Trešdiena(3) ; Ceturtdiena(4) ; Piektdiena(5) ; Sestdiena(6) ; Svētdiena(7)")
+
+first_day = int(input("Ievadi numuru: "))
+cal = calendar.Calendar(firstweekday=first_day)
+
+print(f"\nJūs norādījāt, nedēļas pirmā diena ir: {cal.getfirstweekday()}")
+
+"""
+Funkcija ļauj ievadīt un saglabāt , pretēji - izvadīt, nedēļas pirmo dienu kā skaitli.
+"""
---
+++ 2_.py
@@ -0,0 +1,11 @@
+import calendar
+
+obj = calendar.Calendar(firstweekday = 0)
+
+for day in obj.iterweekdays():
+    print(day)
+
+
+"""
+Funkcija ļauj ievadīt un saglabāt , pretēji - izvadīt, nedēļas pirmo dienu kā skaitli.
+"""
